c1919a737c42ce8cf07c952e67805ec4ff5f74c8
dklarin <danijel.klarin@gmail.com>
2024-08-05 23:54:56 +0200

    Novi dan izmijenjen

--- komponente/bolji_zivot_dodaj.py
+++ komponente/bolji_zivot_dodaj.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
+import datetime
+from collections import Counter
 
 from metode.sheets import plahte
 
@@ -6,9 +8,12 @@ dodaj_dan = Blueprint('dodaj_dan', __name__,
                       template_folder='templates')
 
 worksheet = plahte()
+trenutni_datum = datetime.date.today()
+
+datum = datetime.datetime.strptime(str(trenutni_datum), '%Y-%m-%d')
+formatiran_datum = datum.strftime('%d.%m.%Y')
 
 @dodaj_dan.route('/novi_dan/', methods=['get', 'post'])
-
 def novi_dan():
     
     stupac_redni_broj = [value for value in worksheet.get_col(1) if value]
@@ -24,43 +29,68 @@ def novi_dan():
     stupac_orasi = {int(element) for element in stupac_orasi} # pretvaranje elemenata iz str u int 
     stupac_orasi = sorted(stupac_orasi)
     
-    bioaktiv = ['Da', 'Ne']    
-  
+    dane = ['Da', 'Ne']    
+    
+    # Treći stupac (stupac C) - pronađite zadnji popunjeni redak
+    col_values = worksheet.get_col(3, include_tailing_empty=False)  # Broj 3 označava treći stupac (C)
+    last_row = len(col_values)  # Zadnji popunjeni redak
+    
+    print(last_row)
+    okini=''
+    
+    if (last_row - 2) % 14 == 0:
+        print('okini nokte')
+        okini = 'okini nokte na šakama'
+    else:
+        print('Ne treba kidati nokte')
+        okini = 'ne treba okiniti nokte na šakama'
+    
+    if (last_row - 2) % 28 == 0:
+        print('okini nokte')
+        stopala = 'okini nokte na stopalima'
+    else:
+        print('Ne treba kidati nokte')
+        stopala = 'ne treba okiniti nokte na stopalima'
+
     
     if request.method == 'POST':
         
         dorucak = request.form['dorucak']
         orasi = request.form['orasi']
-        bioaktiv = request.form['bioaktiv']        
-
-        if orasi == 'Otvori za odabir':
-            orasi = 0    
-        else:
-            orasi = orasi    
-    
-        if dorucak != 'Otvori za odabir':        
-            novi_red = [redni_broj, '05.08.2024', dorucak]
-            worksheet.insert_rows(row=redni_broj + 1, values=[novi_red])
-    
+        bioaktiv = request.form['bioaktiv']    
+        trbusnjaci = request.form['trbusnjaci']      
+        nokti_sake = request.form['nokti_sake']
         
-        elif dorucak == 'Otvori za odabir' and orasi != 0:
+        def new_value(new_value, col):
             col_values = worksheet.get_col(3, include_tailing_empty=False)  # Broj 3 označava treći stupac (C)
             last_row = len(col_values)
             
-            new_value = orasi
-            worksheet.update_value((last_row, 5), new_value)
-        elif dorucak == 'Otvori za odabir' and orasi == 0:
-            col_values = worksheet.get_col(3, include_tailing_empty=False)  # Broj 3 označava treći stupac (C)
-            last_row = len(col_values)
-            
-            new_value = bioaktiv
-            worksheet.update_value((last_row, 6), new_value)   
+            new_value = new_value
+            worksheet.update_value((last_row, col), new_value)  
         
-        else:
-            print('Hi')
       
-             
+            
+     
+         
+           
+            
+       
         
+        otvori ='Otvori za odabir'           
+    
+        if dorucak != otvori:        
+            novi_red = [redni_broj, formatiran_datum, dorucak]
+            worksheet.insert_rows(row=redni_broj + 1, values=[novi_red])        
+        elif dorucak == otvori and orasi != otvori:          
+            new_value(orasi, 5)
+        elif dorucak == otvori and orasi == otvori and bioaktiv != otvori:           
+            new_value(bioaktiv, 6)
+        elif dorucak == otvori and orasi == otvori and bioaktiv == otvori and trbusnjaci != '':        
+            new_value(trbusnjaci, 7)  
+        elif dorucak == otvori and orasi == otvori and bioaktiv == otvori and trbusnjaci == '':        
+            new_value(nokti_sake, 8)       
+        else:
+            print(otvori)        
     
         # Da ne radi dupli POST prilikom osvježavanja stranice
         return redirect('/novi_dan')
@@ -69,4 +99,7 @@ def novi_dan():
                            redni_broj=redni_broj,
                            dorucci=dorucci,
                            stupac_orasi=stupac_orasi,
-                           bioaktiv=bioaktiv)
+                           dane=dane,
+                           okini=okini,
+                           stopala=stopala
+                           )
